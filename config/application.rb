a8e6b47958267057c119d6cc88e511c8b235f4c7
steveryan <steveryan6989@gmail.com>
2021-01-17 17:40:36 -0500

    only load dotenv if development

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+if Rails.env.development?
+  Dotenv::Railtie.load
+end
 
 module GitHubFollowers
   class Application < Rails::Application
