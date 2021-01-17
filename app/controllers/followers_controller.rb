b8daf874d64ea442233eb53a4e0fd63ad3320884
steveryan <steveryan6989@gmail.com>
2021-01-17 16:20:36 -0500

    graphql branch working

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -2,8 +2,8 @@ class FollowersController < ApplicationController
 
   def index
     username = params[:username]
-    rest_url = "http://api.github.com/users/#{username}/followers?per_page=100"
-    graphql_url = "http://api.github.com/graphql"
+    rest_url = "https://api.github.com/users/#{username}/followers?per_page=100"
+    graphql_url = "https://api.github.com/graphql"
     @random_choice = ['rest','graphql'].sample
     if @random_choice == 'rest'
       starting_time = Time.now
@@ -11,7 +11,10 @@ class FollowersController < ApplicationController
       ending_time = Time.now
       @total_time = ending_time - starting_time
     else
-      raise "Please implement the GraphQL query"
+      starting_time = Time.now
+      get_followers_graphql(graphql_url, username)
+      ending_time = Time.now
+      @total_time = ending_time - starting_time
     end
   end
   def new
@@ -19,9 +22,23 @@ class FollowersController < ApplicationController
 
   private
 
+  def get_followers_graphql(url, username, cursor = nil)
+    body = {"query": "{ user(login: \"#{username}\") { followers(first: 100#{", after: \"#{cursor}\"" if cursor}){ nodes{login, url, avatarUrl}, pageInfo{endCursor, hasNextPage} } } } "}
+    headers = { 'Accept': 'application/vnd.github.v3+json', 'Authorization': "Bearer #{ENV['GITHUB_API_TOKEN']}"}
+    options = { followlocation: true, "body": body.to_json, "headers": headers }
+    response = HTTParty.post(url, options)
+    @followers = @followers ? @followers + response.parsed_response.dig('data', 'user', 'followers', 'nodes') : response.parsed_response.dig('data', 'user', 'followers', 'nodes')
+    page_info = response.parsed_response.dig('data', 'user', 'followers', 'pageInfo')
+    if page_info['hasNextPage'] == true
+      cursor = page_info['endCursor']
+      get_followers_graphql(url, username, cursor)
+    end
+  end
+
   def get_followers_rest(url)
     response = HTTParty.get(url, headers: { 'Accept' => 'application/vnd.github.v3+json', 'Authorization' => "Bearer #{ENV['GITHUB_API_TOKEN']}"})
-    @followers = @followers ? @followers + JSON.parse(response.body) : JSON.parse(response.body)
+    p JSON.parse(response.body)
+    @followers = @followers ? @followers + JSON.parse(response.body).map{ |f| f.slice('login', 'html_url', 'avatar_url')} : JSON.parse(response.body).map{ |f| f.slice('login', 'html_url', 'avatar_url')}
     if response.headers[:link]&.include?('rel="next"')
       next_page_url = response.headers[:link].match(/.*<(\S*)>; rel="next"/)[1]
       get_followers_rest(next_page_url)
